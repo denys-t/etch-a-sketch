a9b2a3eb243a59a5f34ad4c3db5a4e5b205dde53
denys-t <tulipadanys@gmail.com>
2022-04-05 10:53:03 +0300

    Added reset button to clear the pixels. Fixed sizing via adding vars instead of fixed values

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-const divContainer = document.createElement('div');
-divContainer.id = "container";
+let gridSize = 16;
+let containerSize = 520;
 
-for (let i = 1; i <= 16; i++){
-    const divRow = document.createElement('div');
-    divRow.id = `row-${i}`;
+const divContainer = document.querySelector("#container");
+divContainer.style = `width: ${containerSize}px; height: ${containerSize}px; border: 1px solid;`;
 
-    for (let j = 1; j <= 16; j++){
+const btnReset = document.querySelector("#reset");
+btnReset.addEventListener('click', resetAll);
+
+createGrid(gridSize);
+
+function createGrid(size){
+    let divSize = containerSize / gridSize;
+
+    for (let i = 1; i <= (size*size); i++){
         const divNew = document.createElement('div');
-        divNew.id = `col-${i}`;
-        divNew.textContent = " ";
-        divNew.style = 'display: inline-block;';
+        divNew.id = `div-${i}`;
+        divNew.style = `display: inline-block; width: ${divSize}px; height: ${divSize}px;`;
         divNew.addEventListener('mouseover', addHover);
-        divRow.appendChild(divNew);
+        divContainer.appendChild(divNew);
     }
-
-    divContainer.appendChild(divRow);
 }
 
-const body = document.querySelector('body');
-body.appendChild(divContainer);
+/* const body = document.querySelector('body');
+body.appendChild(divContainer); */
 
 function addHover(){
     this.classList.add('div-hover');
+}
+
+function resetAll(){
+    const pixels = document.querySelectorAll("#container div");
+    pixels.forEach( (pixel) => {
+        pixel.classList.remove("div-hover");
+    });
 }
